89e36189b483eae20f738406f18aed1460e74fb3
wisdomtooth <cbtsao47@gmail.com>
2019-02-05 18:07:09 -0600

    test cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,6 +4,14 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 
 module.exports = server => {
+  server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
   server.use(cors({ credentials: true, origin: true }));
   server.use(express.json());
   server.use(helmet());

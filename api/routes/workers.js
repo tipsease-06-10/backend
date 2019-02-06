ca94b76942a6f4a1ebf17711f1c0e3a751fa0abf
wisdomtooth <cbtsao47@gmail.com>
2019-02-05 23:08:27 -0600

    add put upload

--- api/routes/workers.js
+++ api/routes/workers.js
@@ -54,6 +54,31 @@ route.post("/:id/upload", multerUploads, async (req, res) => {
     });
   }
 });
+route.put("/:id/upload", multerUploads, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const file = dataUri(req).content;
+    const result = await uploader.upload(file);
+    const imgUrl = result.url;
+    await db("workers")
+      .where({ id })
+      .first()
+      .update({ profile_photo: imgUrl });
+    if (req.file) {
+      res.status(200).json({
+        message: `image has been uploaded`,
+        data: { imgUrl }
+      });
+    } else {
+      res.status(400).json({ message: "no file was provided" });
+    }
+  } catch (err) {
+    res.status(400).json({
+      message: `something went wrong while processing the request`,
+      data: { err }
+    });
+  }
+});
 route.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {

3474e48cb3dfcdae2f0b477e07b6d3c070ae7371
wisdomtooth <cbtsao47@gmail.com>
2019-02-05 19:40:54 -0600

    fixed worker list order

--- api/routes/workers.js
+++ api/routes/workers.js
@@ -21,7 +21,8 @@ route.get("/", async (req, res) => {
         "workers.tagline",
         "workers.user_type",
         "workers.occupation"
-      );
+      )
+      .orderBy("workers.id");
     res.status(200).json(workers);
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error", err: err });
